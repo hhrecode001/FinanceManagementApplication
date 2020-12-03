c0365daa2be1e13839e8eda4ca809b6e91a60536
hhrecode001 <57402661+hhrecode001@users.noreply.github.com>
2020-12-03 21:39:02 +0700

    Add files via upload

---
+++ quanlythuphidonggop/QuanLyThuPhiDongGop.java
@@ -0,0 +1,17 @@
+package quanlythuphidonggop;
+
+import views.MainFrame;
+
+
+/**
+ *
+ * @author hoang
+ */
+public class QuanLyThuPhiDongGop {
+
+    public static void main(String[] args) {
+        MainFrame loginUI = new MainFrame();
+        loginUI.setVisible(true); 
+    }
+    
+}
